903505f7a139a3c29611f959579123d686fbabe7
sgavr <sg2295@bath.ac.uk>
2021-02-07 19:06:51 +0200

    Added DFS and cleaned existing code

--- Game.py
+++ Game.py
@@ -1,11 +1,33 @@
 import Node
 
 
+def generate_final_path(node):
+    final_path = []
+    while node is not None:
+        final_path.append(node)
+        node = node.parent
+    return final_path
+
+
+def print_path(path, reverse=False, spec_char=""):
+    if reverse:
+        path = reversed(path)
+    for node in path:
+        print(spec_char + "Exploring: ", node)
+
+
 class Game:
     def __init__(self):
+        """
+
+        """
         self.initial_node = Node.Node()
 
     def breadth_first_search(self):
+        """
+
+        :return:
+        """
         frontier = [self.initial_node]  # Queue
         explored = []
         generated = 0  # Keeps track of how many states we generated
@@ -13,14 +35,12 @@ class Game:
         current_state = frontier.pop(0)  # Dequeue first element from queue
         while not current_state.is_goal_state():
             explored.append(current_state)  # Add the current element to the explored list
-            print("Exploring: ", current_state)
             actions = current_state.get_possible_actions()  # Get all possible actions
             for action in actions:
                 generated += 1
                 new_state = current_state.get_child_node(action)
                 if new_state not in explored and new_state not in frontier and new_state is not None:
-                    frontier.append(
-                        new_state)  # If the action is not in the explored list or already in the frontier, add it
+                    frontier.append(new_state)  # If the action is not in the explored list or already in the frontier, add it
 
             if len(frontier) == 0:  # If the frontier is empty, return failure - no solution
                 print("No solution found")
@@ -28,21 +48,41 @@ class Game:
 
             current_state = frontier.pop(0)  # Move on to the next state
 
-        print("Solution found!")
-        print(f"Explored {len(explored)} states")  # Number of explored states
-        print(f"Generated {generated} states, \n")  # Number of states we generated
-        # Generate the successful path:
-        final_path = []
-        while current_state is not None:
-            final_path.append(current_state)
-            current_state = current_state.parent
-        for state in reversed(final_path):
-            print("Exploring: ", state)
+        print("Solution found! " + f"Explored {len(explored)} states. " + f"Generated {generated} states")
 
-        return final_path[0]
+        return generate_final_path(current_state)
+
+    def depth_first_search(self):
+        frontier = [self.initial_node]  # Stack
+        explored = []
+        generated = 0  # Keeps track of how many states we generated
+
+        current_state = frontier.pop()  # Pop element from the stack
+        while not current_state.is_goal_state():
+            explored.append(current_state)  # Add the current element to the explored list
+            actions = current_state.get_possible_actions()  # Get all possible actions
+            for action in actions:
+                generated += 1
+                new_state = current_state.get_child_node(action)
+                if new_state not in explored and new_state not in frontier and new_state is not None:
+                    frontier.append(new_state)  # If the action is not in the explored list or already in the frontier, add it
+
+            if len(frontier) == 0:  # If the frontier is empty, return failure - no solution
+                print("No solution found")
+                return None
+
+            current_state = frontier.pop()  # Move on to the next state
+
+        print("Solution found! " + f"Explored {len(explored)} states. " + f"Generated {generated} states")
+
+        return generate_final_path(current_state)
 
 
 if __name__ == '__main__':
     g = Game()
-    goal_node = g.breadth_first_search()
-    print("The goal node is ", goal_node)
+    print("Breadth-First Search:")
+    path = g.breadth_first_search()
+    print_path(path, True, "\t")
+    print("\nDepth-First Search: ")
+    path = g.depth_first_search()
+    print_path(path, True, "\t")
--- Node.py
+++ Node.py
@@ -1,4 +1,9 @@
 def is_valid(state):
+    """
+
+    :param state:
+    :return:
+    """
     if (0 <= state[0] <= 3) and (0 <= state[1] <= 3) and (0 <= state[2] <= 1):  # Check values are valid
         if state[0] == 0 or state[0] >= state[1]:
             # No missionaries or the number of missionaries is greater than or equal to cannibals
@@ -8,16 +13,35 @@ def is_valid(state):
 
 class Node:
     def __init__(self, m_wrong_side=3, c_wrong_side=3, boat_wrong_side=1, parent=None):
+        """
+
+        :param m_wrong_side:
+        :param c_wrong_side:
+        :param boat_wrong_side:
+        :param parent:
+        """
         self.state = (m_wrong_side, c_wrong_side, boat_wrong_side)  # Num of m, c, boat side
         self.parent = parent
 
     def __str__(self):
+        """
+
+        :return:
+        """
         return str(self.state)
 
     def is_goal_state(self):
+        """
+
+        :return:
+        """
         return self.state == (0, 0, 0)
 
     def __eq__(self, other):
+        """
+        :param other:
+        :return:
+        """
         if other is None:
             return False
         for i in range(0, 3):
@@ -26,15 +50,16 @@ class Node:
         return True
 
     def get_possible_actions(self):
-        """""
+        """
         move one M (1,0,1): do - to go from left to right and + tp gp from right to left
         move two M (2,0,1): do - to go right and + to go left
         move one M and one C (1,1,1): do - to go right and + to go left
         move one C (0,1,1)
         move two C (0,2,1)
-        """""
-        actions = [(1, 0, 1), (2, 0, 1), (1, 1, 1), (0, 1, 1), (0, 2, 1)]
-        possible_actions = []
+        :return:
+        """
+        actions = [(1, 0, 1), (2, 0, 1), (1, 1, 1), (0, 1, 1), (0, 2, 1)]  # List of actions we can do
+        possible_actions = []  # List of available (possible) actions
         for action in actions:
             new_state_left = self.get_resulting_state(action)
             new_m_right = 3 - new_state_left[0]
@@ -46,6 +71,11 @@ class Node:
         return possible_actions
 
     def get_resulting_state(self, action):
+        """
+
+        :param action:
+        :return:
+        """
         if self.state[2] == 1:
             new_m = self.state[0] - action[0]
             new_c = self.state[1] - action[1]
@@ -57,5 +87,10 @@ class Node:
         return new_m, new_c, new_b
 
     def get_child_node(self, action):
+        """
+
+        :param action:
+        :return:
+        """
         new_state = self.get_resulting_state(action)
-        return self.__class__(new_state[0], new_state[1], new_state[2], parent=self)  # Return resulting node (Node after the action)
+        return self.__class__(new_state[0], new_state[1], new_state[2], parent=self)  # Return resulting node
